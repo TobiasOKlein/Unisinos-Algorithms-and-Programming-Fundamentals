a4b28aa19685fd2345159951cc3c6e60e094ba71
TobiasOKlein <152293349+TobiasOKlein@users.noreply.github.com>
2024-09-25 20:15:06 -0300

    Add files via upload

---
+++ List2/Ex4.c
@@ -0,0 +1,32 @@
+/*
+ * Author: Tobias Klein
+ *
+ * Exercise: 4. Faça um algoritmo que permita ao aluno calcular
+ * a sua média final na Unisinos. Leia a nota do grau A e do grau B
+ * e escreva o resultado na tela. Lembrando que o Grau A vale
+ * 1/3 e o Grau B 2/3.
+ * 
+ */
+
+#include <stdio.h>
+
+int main(){
+
+    float notaGA, notaGB, mediaFinal;
+
+    printf("\n Calculo de media final Unisinos \n Digite sua nota do Grau A : ");
+    scanf("%f", &notaGA);
+    printf("\n Digite sua nota do Grau B : ");
+    scanf("%f", &notaGB);
+    
+    if(!(0<=notaGA && notaGA<=10 && 0<=notaGB && notaGB<=10)){
+        printf("\n Nota invalida, sua nota deve ser entre 0 e 10.");
+        return 0;
+    }
+
+    mediaFinal = notaGA/3 + notaGB*2/3;
+
+    printf("\n Sua media final foi de %.2f pontos.", mediaFinal);
+ 
+    return 0;
+}
